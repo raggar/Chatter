e42f200ac012488283b10cb1dd0c6d81c1e3c084
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-01-08 16:40:17 -0500

    minor

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    twilio: {
-      accountSid: "ACbe9999406182e94ec2b3348a68b0e021",
-      apiKey: "SK6a3bddf95a147af7bed2ffbede465fe9",
-      apiSecret: "ayfMaCRbtYoarwcDILrr8N7f7pzv4b1F",
-    }
-  };
+  twilio: {
+    accountSid: process.env.ACCOUNTSID,
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET,
+  },
+};
